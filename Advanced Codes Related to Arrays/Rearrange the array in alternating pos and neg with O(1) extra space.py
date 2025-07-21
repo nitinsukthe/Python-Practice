95e7b4c124f9dc26aa7d3f1655fe34ef9cf23906
Nitin Sukthe <sukthenitin@gmail.com>
2025-07-21 17:59:39 +0530

    Create Rearrange the array in alternating pos and neg with O(1) extra space.py

---
+++ Advanced Codes Related to Arrays/Rearrange the array in alternating pos and neg with O(1) extra space.py
@@ -0,0 +1,14 @@
+def rearrange(arr):
+    p=0
+    b=0
+    for i in range(len(arr)):
+        if b==1:
+            b-=1
+        elif arr[i]<0:
+            arr[i],arr[p]=arr[p],arr[i]
+            if p>i:
+                b+=1
+            p+=2
+    return arr
+array=[2,3,-1,-4,6,-9]
+print(f"After Rearranging: {rearrange(array)}")
