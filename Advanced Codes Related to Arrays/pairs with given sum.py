d9804e8bc2c367e227fc74e0e03ee015a04f3fba
Nitin Sukthe <sukthenitin@gmail.com>
2025-07-22 18:57:26 +0530

    Create pairs with given sum.py

---
+++ Advanced Codes Related to Arrays/pairs with given sum.py
@@ -0,0 +1,10 @@
+def find(array, len, summ):
+    print("Pairs whose sum is : ", summ)
+    for i in range(len):
+        for j in range(i, len):
+            if (array[i] + array[j]) == summ:
+                print(array[i], array[j])
+array = [5, 2, 3, 4, 1, 6, 7]
+summ = 7
+print("Array= ", array)
+find(array, len(array), summ)
