bb47eabe42d70523c90fb7e12025b760e46c9ea4
Nitin Sukthe <sukthenitin@gmail.com>
2025-07-29 23:59:28 +0530

    Create problem4.py

---
+++ Easy and simple/problem4.py
@@ -0,0 +1,20 @@
+# Print multiplication table using while loop.
+
+n = int(input("Ener a number: "))
+i=1
+while (i<11):
+  print(f"{n} x {i} = {n*i}")
+  i+=1
+
+# output:
+# Ener a number: 2
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
+# 2 x 6 = 12
+# 2 x 7 = 14
+# 2 x 8 = 16
+# 2 x 9 = 18
+# 2 x 10 = 20
