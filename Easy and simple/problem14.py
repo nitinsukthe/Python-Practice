439622ec40d527b02a855383a945892e9a46e9de
Nitin Sukthe <sukthenitin@gmail.com>
2025-08-11 21:20:27 +0530

    Create problem14.py

---
+++ Easy and simple/problem14.py
@@ -0,0 +1,22 @@
+# WORDLIST GENERATOR
+
+base_words = ["admin", "123", "pass", "root"]
+
+for word in base_words:
+    print(word + "123")
+    print(word + "2025")
+    print(word.upper())
+
+# output:
+# admin123
+# admin2025
+# ADMIN
+# 123123
+# 1232025
+# 123
+# pass123
+# pass2025
+# PASS
+# root123
+# root2025
+# ROOT
