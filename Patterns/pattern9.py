dfdcfa886a44967aef71707e920268e479a326bb
Nitin Sukthe <sukthenitin@gmail.com>
2025-08-15 17:32:54 +0530

    Create pattern9.py

---
+++ Patterns/pattern9.py
@@ -0,0 +1,23 @@
+# * * * *  
+# *       *  
+# *       *  
+# * * * *  
+# *       *  
+# *       *  
+# * * * *  
+
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if j == 0 or \
+           (i == 0 and j < cols-1) or \
+           (i == rows//2 and j < cols-1) or \
+           (i == rows-1 and j < cols-1) or \
+           (j == cols-1 and i != 0 and i != rows//2 and i != rows-1):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+  
