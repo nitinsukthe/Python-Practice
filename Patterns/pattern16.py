75a6ff5b25f9f29ba617df9bbab89a12a800c5ae
Nitin Sukthe <sukthenitin@gmail.com>
2025-08-18 21:27:45 +0530

    Create pattern16.py

---
+++ Patterns/pattern16.py
@@ -0,0 +1,18 @@
+# * * * * * 
+#     *     
+#     *     
+#     *     
+#     *     
+#     *     
+# * * * * * 
+
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if i == 0 or i == rows-1 or j == cols//2:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
