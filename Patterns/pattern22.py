d287d5bb26be7f4382edb187625eb6a1e5fc62fb
Nitin Sukthe <sukthenitin@gmail.com>
2025-08-24 14:58:02 +0530

    Create pattern22.py

---
+++ Patterns/pattern22.py
@@ -0,0 +1,20 @@
+#   * * *   
+# *       * 
+# *       * 
+# *       * 
+# *       * 
+# *       * 
+#   * * *   
+
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if (i == 0 or i == rows-1) and (j != 0 and j != cols-1):
+            print("*", end=" ")
+        elif (j == 0 or j == cols-1) and (i != 0 and i != rows-1):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
