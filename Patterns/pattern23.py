53ffbfee301bfd09cafce702094a3721b313921a
Nitin Sukthe <sukthenitin@gmail.com>
2025-08-25 21:19:21 +0530

    Create pattern23.py

---
+++ Patterns/pattern23.py
@@ -0,0 +1,24 @@
+# * * * *   
+# *       * 
+# *       * 
+# * * * *   
+# *         
+# *         
+# *    
+
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if j == 0:
+            print("*", end=" ")
+        elif i == 0 and j < cols-1:
+            print("*", end=" ")
+        elif i == rows//2 and j < cols-1:
+            print("*", end=" ")
+        elif j == cols-1 and i > 0 and i < rows//2:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
