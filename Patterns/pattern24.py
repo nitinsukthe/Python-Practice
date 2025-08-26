d1e1d4d249f998e37bc50d8d1bf73c802329c899
Nitin Sukthe <sukthenitin@gmail.com>
2025-08-26 21:37:20 +0530

    Create pattern24.py

---
+++ Patterns/pattern24.py
@@ -0,0 +1,22 @@
+#   * * *   
+# *       * 
+# *       * 
+# *     * * 
+# *       * 
+#   * * *   
+# *       * 
+
+rows = 7
+cols = 5
+
+for i in range(rows):
+    for j in range(cols):
+        if (i == 0 or i == rows-2) and (j != 0 and j != cols-1):
+            print("*", end=" ")
+        elif (j == 0 or j == cols-1) and (i != 0 and i != rows-2):
+            print("*", end=" ")
+        elif i == j and i >= rows//2:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
